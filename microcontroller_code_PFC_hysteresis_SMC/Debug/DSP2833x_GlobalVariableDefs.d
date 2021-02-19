d5e120c87f45e1a28d40be0a6e812c7823218992
joseroca1989 <79333326+joseroca1989@users.noreply.github.com>
2021-02-19 14:23:55 -0500

    Delete microcontroller_code_PFC_hysteresis_SMC directory

--- microcontroller_code_PFC_hysteresis_SMC/Debug/DSP2833x_GlobalVariableDefs.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-DSP2833x_GlobalVariableDefs.obj: ../DSP2833x_GlobalVariableDefs.c
-
-../DSP2833x_GlobalVariableDefs.c: 
--- microcontroller_code_PFC_hysteresis_SMC/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/ti/controlSUITE/libs/app_libs/solar/v1.2/float/lib/Solar_Lib_Float.lib"
-
